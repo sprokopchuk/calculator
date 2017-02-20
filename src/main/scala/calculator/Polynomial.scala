b742a19af323413bba87251f34a988873d0e8a59
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2017-02-20 21:55:31 +0200

    Implement some functions

--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,22 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var(b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal(
+      if(delta() < 0) {
+        Set()
+      } else if(delta() == 0) {
+        Set(- b() / (2 * a()))
+      }
+      else {
+        val sol1 = (- b() + delta()) / (2 * a())
+        val sol2 = (- b() - delta()) / (2 * a())
+        Set(sol1, sol2)
+      }
+    )
   }
 }
--- src/main/scala/calculator/TweetLength.scala
+++ src/main/scala/calculator/TweetLength.scala
@@ -4,11 +4,15 @@ object TweetLength {
   final val MaxTweetLength = 140
 
   def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
-    ???
+    Signal(MaxTweetLength - tweetLength(tweetText()))
   }
 
   def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
-    ???
+    Signal(
+      if(remainingCharsCount() >= 15) "green"
+      else if(remainingCharsCount() < 14 && remainingCharsCount() >= 0) "orange"
+      else "red"
+    )
   }
 
   /** Computes the length of a tweet, given its text string.
